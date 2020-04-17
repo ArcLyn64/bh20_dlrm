9f800ff9e7dabd443115de0f78ab957404c03665
ArclingML <parthpatel6400@gmail.com>
2020-04-16 22:54:47 -0700

    pawns no longer move into death; big brain ensues

--- shalloworange/DlrmPawn.py
+++ shalloworange/DlrmPawn.py
@@ -2,6 +2,8 @@ DEBUG = 1
 def dlog(str):
     if DEBUG > 0: log(str)
 
+VIEW_DIST = 2
+
 class Pawn:
 
     def __init__(self, bs, t, l):
@@ -16,15 +18,22 @@ class Pawn:
         self.right = self.forward * -1
 
     def check_space_w(self, r, c):
-        if not self.check_inbounds(r, c):
+        if not self.check_inbounds(r, c) or not self.check_inview(r, c):
             return False
         try:
             return check_space(r, c)
         except:
             return None
 
+    def check_loc(self, loc):
+        return self.check_space_w(loc[0], loc[1])
+
     def check_inbounds(self, r, c):
         return 0 <= r < self.board_size and 0 <= c < self.board_size
+
+    def check_inview(self, r, c):
+        rpos, cpos = self.loc
+        return -VIEW_DIST <= r - rpos <= VIEW_DIST and -VIEW_DIST <= c-cpos <= VIEW_DIST
 
     def try_move(self):
         r, c = self.loc
@@ -33,6 +42,21 @@ class Pawn:
             move_forward()
             return True
         return False
+
+    def space_safe(self, r, c):
+        if not self.check_inview(r, c):
+            return None
+        # check if the space is defended by friendly pawn
+        friend_left = (r - self.forward, c + self.left)
+        friend_right = (r - self.forward, c + self.right)
+        if self.check_loc(friend_left) == self.team or self.check_loc(friend_right) == self.team:
+            return True
+        # check if the space is under attack
+        enemy_left = (r + self.forward, c + self.left)
+        enemy_right = (r + self.forward, c + self.right)
+        if self.check_loc(enemy_left) == self.opp_team or self.check_loc(enemy_right) == self.opp_team:
+            return False
+        return True
 
     def try_capture_left(self):
         return self.try_capture(False)
@@ -52,4 +76,4 @@ class Pawn:
     def turn(self):
         if self.try_capture_left(): return True
         if self.try_capture_right(): return True
-        return self.try_move()
+        if self.space_safe(self.loc[0] + self.forward, self.loc[1]): self.try_move()
